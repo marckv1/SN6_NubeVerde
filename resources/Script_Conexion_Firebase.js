e4b7825284256d6b8ab4b3aa5074d96be1fbf3d6
LMarrokings <luismarrokings@gmail.com>
2025-12-17 21:09:25 -0600

    Les comparto es script de conexion con la APikey cambiada por seguridad
    Este es el script de conexión, por seguridad he cambiado el apikey y otros datos, les estaré proporcionando los valores reales en interno

---
+++ resources/Script_Conexion_Firebase.js
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "BIzaSyAE_pViacv5LR9DpalknyS5nuu",
+  authDomain: "nube-verde-monitor.firebaseapp.com",
+  projectId: "nube-verde-monitor",
+  storageBucket: "nube-verde-monitor.firebasestorage.app",
+  messagingSenderId: "694437356246",
+  appId: "1:694437356246:web:0b6792fd2a913727739f77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+************************************************
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "BAIzaSyAE_pViacv5LR9DpalknyS5nuu",
+    authDomain: "nube-verde-monitor.firebaseapp.com",
+    projectId: "nube-verde-monitor",
+    storageBucket: "nube-verde-monitor.firebasestorage.app",
+    messagingSenderId: "694437356246",
+    appId: "1:694437356246:web:0b6792fd2a913727739f77"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
